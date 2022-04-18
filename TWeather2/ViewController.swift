b877c53fab41271db8a09ac926678c339b9e0302
vlad <fallingdown485@gmail.com>
2022-04-18 16:23:16 +0300

    Initial Commit

---
+++ TWeather2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TWeather2
+//
+//  Created by Vladislav Tuleiko on 18.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TWeather2UITests/TWeather2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TWeather2UITestsLaunchTests.swift
+//  TWeather2UITests
+//
+//  Created by Vladislav Tuleiko on 18.04.22.
+//
+
+import XCTest
+
+class TWeather2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
